02ee9861157324b00abdf833c3f5776c4463d18e
章博 <brandonfaustine@gmail.com>
2022-01-12 20:11:32 +0800

    追加 dissmiss onTapDown

--- lib/kumi_popup_window.dart
+++ lib/kumi_popup_window.dart
@@ -1057,7 +1057,7 @@ class _KumiPopupWindowState extends State<KumiPopupWindow> with SingleTickerProv
                       color: widget._bgColor,
                     ),
                   ),
-                  onTap: () {
+                  onTapDown: (detail) {
                     if (widget._clickOutDismiss) {
                       widget.dismiss(context);
                       return;
